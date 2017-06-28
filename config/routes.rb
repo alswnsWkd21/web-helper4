780d6cc93bf6bb372e037a9ecdf73b356af1ab8b
alswnsWkd21 <djflqjfl0209@gmail.com>
2017-06-28 14:28:57 +0000

    step3 검색 전

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'posts/index'
+
   resources :messages
   devise_for :users
   get 'comments/create'
@@ -8,11 +10,15 @@ Rails.application.routes.draw do
   get 'helper/index'
 
   resources :projects
-  root "projects#index"
+  # root "projects#index"
   get "projects/index"
   get "projects/new" => "projects#new"
   get "projects/show/:project_id" => "projects#set_project"
   
   post "projects/show/:project_id/comments/create" => "comments#create"
   post "projects/show/:project_id/comments/delete/:comment_id" => "comments#delete"
+  
+  Rails.application.routes.draw do
+    root to: "posts#index"
+  end
 end
